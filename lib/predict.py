9880df052af1e2b7baa6d1d1d11a15a736ef5eac
Demetrius <meetri@gmail.com>
2023-06-21 12:03:03 -0700

    latest

--- lib/predict.py
+++ lib/predict.py
@@ -1,8 +1,44 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+from torch.utils.data import Dataset
 import numpy as np
 import torch
 import torch.nn as nn
+import plotly.graph_objects as go
+
+class SequenceDataset(Dataset):
+
+  def __init__(self, df):
+    self.data = df
+
+  def __getitem__(self, idx):
+    sample = self.data[idx]
+    return torch.Tensor(sample['sequence']), torch.Tensor(sample['target'])
+  
+  def __len__(self):
+    return len(self.data)
+
+def generate_sequences(df: pd.DataFrame, tw: int, pw: int, target_columns, inplace=False, drop_targets=False):
+  '''
+  df: Pandas DataFrame of the univariate time-series
+  tw: Training Window - Integer defining how many steps to look back
+  pw: Prediction Window - Integer defining how many steps to predict
+
+  returns: dictionary of sequences and targets for all sequences
+  '''
+  data = dict() # Store results into a dictionary
+  L = len(df)
+  for i in range(L-tw):
+    # Option to drop target from dataframe
+    if drop_targets:
+      df.drop(target_columns, axis=1, inplace=True)
+
+    # Get current sequence  
+    sequence = df[i:i+tw][target_columns].values
+    # Get values right after the current sequence
+    target = df[i+tw:i+tw+pw][target_columns].values
+    data[i] = {'sequence': sequence, 'target': target}
+  return data
 
 def split_data(stock, lookback):
     data_raw = stock.to_numpy() # convert to numpy array
@@ -23,6 +59,164 @@ def split_data(stock, lookback):
     y_test = data[train_set_size:,-1,:]
     
     return [x_train, y_train, x_test, y_test]
+
+
+class Forecaster:
+
+  def __init__(self, model, data, target, tw):
+    self.model = model
+    self.data = data
+    self.tw = tw
+    self.target = target
+
+  def plot_forecast(self, history):
+    fig = go.Figure()
+    # Add traces
+    fig.add_trace(go.Scatter(x=history.index, y=history.actual,
+                        mode='lines',
+                        name='actual'))
+    fig.add_trace(go.Scatter(x=history.index, y=history.forecast,
+                        mode='lines',
+                        name='forecast'))
+    fig.update_layout(
+    autosize=False,
+    width=2400,
+    height=800,)
+    fig.show()
+  
+  def one_step_forecast(self, history):
+      '''
+      history: a sequence of values representing the latest values of the time 
+      series, requirement -> len(history.shape) == 2
+
+      outputs a single value which is the prediction of the next value in the
+      sequence.
+      '''
+      self.model.cpu()
+      self.model.eval()
+      with torch.no_grad():
+        pre = torch.Tensor(history).unsqueeze(0)
+        pred = self.model(pre)
+      return pred.detach().numpy().reshape(-1)
+
+  def n_step_forecast(self, n: int, forecast_from: int=None, plot=False):
+      '''
+      n: integer defining how many steps to forecast
+      forecast_from: integer defining which index to forecast from. None if
+      you want to forecast from the end.
+      plot: True if you want to output a plot of the forecast, False if not.
+      '''
+      history = self.data[self.target] # .to_frame()
+    
+      # print(history)
+      # Create initial sequence input based on where in the series to forecast 
+      # from.
+      if forecast_from:
+        pre = list(history[forecast_from - self.tw : forecast_from][self.target].values)
+      else:
+        pre = history[self.target].values[-self.tw:].tolist()
+        
+      # Call one_step_forecast n times and append prediction to history
+      for i, step in enumerate(range(n)):
+        pre_ = np.array(pre[-self.tw:]).reshape(-1, 1)
+        forecast = self.one_step_forecast(pre_).squeeze()
+        pre.append(forecast)
+
+      res = history.copy()
+      ls = [np.nan for i in range(len(history))]
+
+      # Note: I have not handled the edge case where the start index + n crosses
+      # the end of the dataset
+      if forecast_from:
+        ls[forecast_from : forecast_from + n] = list(np.array(pre[-n:]))
+        res['forecast'] = ls
+        res.columns = ['actual', 'forecast']
+      else:
+        fc = ls + list(np.array(pre[-n:]))
+        ls = ls + [np.nan for i in range(len(pre[-n:]))]
+        ls[:len(history)] = history[self.target].values
+        res = pd.DataFrame([ls, fc], index=['actual', 'forecast']).T
+
+      if plot:
+        self.plot_forecast(res)
+      return res
+
+
+def make_predictions_from_dataloader(model, dataloader):
+  model.eval()
+  predictions, actuals = [], []
+  for x, y in unshuffled_dataloader:
+    with torch.no_grad():
+      p = model(x)
+      predictions.append(p)
+      actuals.append(y.squeeze())
+  predictions = torch.cat(predictions).numpy()
+  actuals = torch.cat(actuals).numpy()
+  return predictions.squeeze(), actuals
+
+class LSTMForecaster(nn.Module):
+
+  def __init__(self, n_features, n_hidden, n_outputs, sequence_len, n_lstm_layers=1, n_deep_layers=10, use_cuda=False, dropout=0.2):
+    '''
+    n_features: number of input features (1 for univariate forecasting)
+    n_hidden: number of neurons in each hidden layer
+    n_outputs: number of outputs to predict for each training example
+    n_deep_layers: number of hidden dense layers after the lstm layer
+    sequence_len: number of steps to look back at for prediction
+    dropout: float (0 < dropout < 1) dropout ratio between dense layers
+    '''
+    super().__init__()
+
+    self.n_lstm_layers = n_lstm_layers
+    self.nhid = n_hidden
+    self.use_cuda = use_cuda # set option for device selection
+
+    # LSTM Layer
+    self.lstm = nn.LSTM(n_features,
+                        n_hidden,
+                        num_layers=n_lstm_layers,
+                        batch_first=True) # As we have transformed our data in this way
+    
+    # first dense after lstm
+    self.fc1 = nn.Linear(n_hidden * sequence_len, n_hidden) 
+    # Dropout layer 
+    self.dropout = nn.Dropout(p=dropout)
+
+    # Create fully connected layers (n_hidden x n_deep_layers)
+    dnn_layers = []
+    for i in range(n_deep_layers):
+      # Last layer (n_hidden x n_outputs)
+      if i == n_deep_layers - 1:
+        dnn_layers.append(nn.ReLU())
+        dnn_layers.append(nn.Linear(self.nhid, n_outputs))
+      # All other layers (n_hidden x n_hidden) with dropout option
+      else:
+        dnn_layers.append(nn.ReLU())
+        dnn_layers.append(nn.Linear(self.nhid, self.nhid))
+        if dropout:
+          dnn_layers.append(nn.Dropout(p=dropout))
+    # compile DNN layers
+    self.dnn = nn.Sequential(*dnn_layers)
+
+  def forward(self, x):
+
+    # Initialize hidden state
+    hidden_state = torch.zeros(self.n_lstm_layers, x.shape[0], self.nhid)
+    cell_state = torch.zeros(self.n_lstm_layers, x.shape[0], self.nhid)
+
+    # move hidden state to device
+    if self.use_cuda:
+      hidden_state = hidden_state.to(device)
+      cell_state = cell_state.to(device)
+        
+    self.hidden = (hidden_state, cell_state)
+
+    # Forward Pass
+    x, h = self.lstm(x, self.hidden) # LSTM
+    x = self.dropout(x.contiguous().view(x.shape[0], -1)) # Flatten lstm out 
+    x = self.fc1(x) # First Dense
+    return self.dnn(x) # Pass forward through fully connected DNN.
+
 
 class LSTM(nn.Module):
     def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
