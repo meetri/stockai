e90a9dac01780d89f2e1e0e09cefba189aef64c8
Demetrius Bell <dbell@convergeins.com>
2023-06-22 07:15:11 -0700

    tuning model

--- lib/forecast.py
+++ lib/forecast.py
@@ -1,25 +1,28 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from lib.predict import split_sequences
 import torch
-# import torch.nn as nn
 from torch.autograd import Variable
+from torch.utils.data import DataLoader, random_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from lib.lstm import LSTM
+from lib.helpers import SequenceDataset, x_split_sequences, split_sequences
 
 
 class StockForecaster:
 
-    def __init__(self):
-        self.epochs = 1000
-        self.learning_rate = 0.001  # 0.001 lr
+    def __init__(self, epochs=200, lr=0.001):
+        self.epochs = epochs
+        self.learning_rate = lr
 
-        self.input_size = 4  # number of features
-        self.hidden_size = 2  # number of features in hidden state
+        self.train_on = ["Open", "High", "Low", "Close", "Volume"]
+        self.target = "Close"
+
+        self.input_size = len(self.train_on) - 1  # number of features
+        self.hidden_size = 20  # number of features in hidden state
         self.num_layers = 1  # number of stacked lstm layers
-        self.num_classes = 50  # number of output classes
+        self.num_classes = 20  # number of output classes
 
-        self.num_training_sequences = 100
-        self.num_predictions = 50
+        self.num_training_sequences = 200
+        self.num_predictions = 20
         self.percent_for_training = 0.9
 
         self.total_samples = None
@@ -29,6 +32,9 @@ class StockForecaster:
         # fitted training data full dataset
         self.X_train = None
         self.y_train = None
+
+        self.trainloader = None
+        self.testloader = None
 
         self.X_ss = None
         self.y_mm = None
@@ -49,40 +55,65 @@ class StockForecaster:
     def load(self, csvpath: str = "spydata.csv"):
         self.raw_data = pd.read_csv(
             csvpath, index_col='Date', parse_dates=True)
-        self.raw_data.drop(columns=['Adj Close'], inplace=True)
+
+        self.raw_data = self.raw_data[self.train_on]
 
         # calculate train / test sample count
-        self.total_samples = len(self.raw_data.Close.values)
+        self.total_samples = len(self.raw_data[self.target].values)
+
         self.train_samples = round(
             self.total_samples * self.percent_for_training)
         self.test_samples = self.total_samples - self.train_samples
 
     def normalize_data(self):
-        # X contains all columns except `Close`
+        # X contains all columns except `Close` ( self.target )
         # Y contains only `Close`
         X, y = (
-            self.raw_data.drop(columns=['Close']),
+            self.raw_data.drop(columns=[self.target]),
             self.raw_data.Close.values.reshape(-1, 1)
         )
 
         self.X_train = self.ss.fit_transform(X)
         self.y_train = self.mm.fit_transform(y)
 
-        p = self.prepare()
-        self.create_tensors(
-            X_train=p[0],
-            X_test=p[1],
-            y_train=p[2],
-            y_test=p[3]
-        )
-
     def prepare(self):
         # prepare data for training...
+
+        self.normalize_data()
+
         self.X_ss, self.y_mm = split_sequences(
             self.X_train, self.y_train,
             self.num_training_sequences, self.num_predictions
         )
 
+        self.create_tensors()
+
+    def prepare2(self):
+        # prepare data for training...
+
+        self.normalize_data()
+
+        sequences = x_split_sequences(
+            self.X_train, self.y_train,
+            self.num_training_sequences, self.num_predictions
+        )
+
+        dataset = SequenceDataset(sequences)
+
+        train_len = int(len(dataset)*self.percent_for_training)
+        lens = [train_len, len(dataset)-train_len]
+
+        train_ds, test_ds = random_split(dataset, lens)
+
+        self.trainloader = DataLoader(
+            train_ds, batch_size=16, shuffle=True, drop_last=True
+        )
+
+        self.testloader = DataLoader(
+            test_ds, batch_size=16, shuffle=True, drop_last=True
+        )
+
+    def create_tensors(self):
         # split data into train / test sets
         X_train = self.X_ss[:-self.test_samples]
         X_test = self.X_ss[-self.test_samples:]
@@ -90,9 +121,6 @@ class StockForecaster:
         y_train = self.y_mm[:-self.test_samples]
         y_test = self.y_mm[-self.test_samples:]
 
-        return X_train, X_test, y_train, y_test
-
-    def create_tensors(self, X_train, X_test, y_train, y_test):
         # create tensors
         self.X_train_tensors = Variable(torch.Tensor(X_train))
         self.X_test_tensors = Variable(torch.Tensor(X_test))
@@ -115,6 +143,31 @@ class StockForecaster:
             lr=self.learning_rate
         )
 
+    def train2(self):
+        loss_data = []
+        test_loss_data = []
+        for epoch in range(self.epochs):
+            self.lstm.train()
+            for x, y in self.trainloader:
+                print("x")
+                self.optimiser.zero_grad()
+
+                outputs = self.lstm(x).squeeze()
+                loss = self.loss_fn(outputs, self.y_train_tensors)
+                loss_data.append(loss.item())
+                loss.backward()
+                self.optimiser.step()
+
+            self.lstm.eval()
+            for x, y in self.testloader:
+                print("y")
+                with torch.no_grad():
+                    output = self.lstm(x)
+                    error = self.loss_fn(output, y)
+                    test_loss_data.append(error.item())
+
+        return loss_data, test_loss_data
+
     def train(self):
         """
         n_epochs, lstm, optimiser, loss_fn, X_train,
@@ -122,6 +175,7 @@ class StockForecaster:
         """
 
         loss_data = []
+        test_loss_data = []
         for epoch in range(self.epochs):
             self.lstm.train()
             outputs = self.lstm.forward(self.X_train_tensors)  # forward pass
@@ -140,9 +194,11 @@ class StockForecaster:
             self.lstm.eval()
             test_preds = self.lstm(self.X_test_tensors)
             test_loss = self.loss_fn(test_preds, self.y_test_tensors)
+            test_loss_data.append(test_loss.item())
+
             if epoch % 100 == 0:
                 print(
                     f"Epoch: {epoch}, train loss: {loss.item()}, "
                     f"test loss: {test_loss.item()}"
                 )
-        return loss_data
+        return loss_data, test_loss_data
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,34 +1,46 @@
 import numpy as np
+from torch.utils.data import Dataset
+import torch
 
 
-class LSTM(nn.Module):
+class SequenceDataset(Dataset):
 
-    def __init__(self, num_classes, input_size, hidden_size, num_layers):
-        super().__init__()
-        self.num_classes = num_classes # output size
-        self.num_layers = num_layers # number of recurrent layers in the lstm
-        self.input_size = input_size # input size
-        self.hidden_size = hidden_size # neurons in each lstm layer
-        # LSTM model
-        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
-                            num_layers=num_layers, batch_first=True ) # , dropout=0.2) # lstm
-        self.fc_1 =  nn.Linear(hidden_size, 128) # fully connected
-        self.fc_2 = nn.Linear(128, num_classes) # fully connected last layer
-        self.relu = nn.ReLU()
+    def __init__(self, df):
+        self.data = df
 
-    def forward(self,x):
-        # hidden state
-        h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
-        # cell state
-        c_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
-        # propagate input through LSTM
-        output, (hn, cn) = self.lstm(x, (h_0, c_0)) # (input, hidden, and internal state)
-        hn = hn.view(-1, self.hidden_size) # reshaping the data for Dense layer next
-        out = self.relu(hn)
-        out = self.fc_1(out) # first dense
-        out = self.relu(out) # relu
-        out = self.fc_2(out) # final output
-        return out
+    def __getitem__(self, idx):
+        sample = self.data[idx]
+        return torch.Tensor(sample['sequence']), torch.Tensor(sample['target'])
+
+    def __len__(self):
+        return len(self.data)
+
+
+def x_split_sequences(
+    input_sequences, output_sequence, n_steps_in, n_steps_out
+):
+    "split a multivariate sequence past, future samples (X and y)"
+
+    data = list()
+    for i in range(len(input_sequences)):
+        # find the end of the input, output sequence
+        end_ix = i + n_steps_in
+        out_end_ix = end_ix + n_steps_out - 1
+        # check if we are beyond the dataset
+        if out_end_ix > len(input_sequences):
+            break
+        # gather input and output of the pattern
+        seq_x, seq_y = (
+            input_sequences[i:end_ix],
+            output_sequence[end_ix-1:out_end_ix, -1]
+        )
+
+        data.append({
+            "sequence": seq_x,
+            "target": seq_y
+        })
+
+    return data
 
 
 def split_sequences(input_sequences, output_sequence, n_steps_in, n_steps_out):
